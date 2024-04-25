7e7cae38466807797240f64879bc4fa2403faaf4
Houssem <h.zitoun@externe.prismamedia.com>
2024-04-25 17:11:08 +0200

    Shippeo-ticket-number: fix Order createdAt

--- srv/app/src/UserInterface/Cli/Mock/CreateOrderCli.php
+++ srv/app/src/UserInterface/Cli/Mock/CreateOrderCli.php
@@ -9,10 +9,11 @@ use App\Application\Command\Order\CreateOrder\CreateOrder;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
- * This cli is used to simulate the sending of a create order message from the outside.
+ * This cli is used to simulate the sending of a creation order message from the outside.
  */
 #[AsCommand(
     name: 'app:mock:order-create',
@@ -20,21 +21,59 @@ use Symfony\Component\Console\Output\OutputInterface;
 )]
 class CreateOrderCli extends Command
 {
+    public const string DATE_FORMAT = 'Y-m-d';
+
     public function __construct(
-        private CommandBus $workerBus,
+        private readonly CommandBus $workerBus,
     ) {
         parent::__construct();
     }
 
+    #[\Override]
     public function execute(InputInterface $input, OutputInterface $output): int
     {
-        $this->workerBus->execute(new CreateOrder(
-            orderReference: 'Reference1',
-            loadingIdentifier: 'place123',
-            deliveryIdentifier: 'place456',
-            creationDate: new \DateTimeImmutable('2024-03-11 09:00:00')
-        ));
+        try {
+            $creationDateTime = $this->checkCreationDate($input->getOption('creationDate'));
 
-        return Command::SUCCESS;
+            $this->workerBus->execute(new CreateOrder(
+                orderReference: 'Reference1',
+                loadingIdentifier: 'place123',
+                deliveryIdentifier: 'place456',
+                creationDate: $creationDateTime
+            ));
+
+            $output->writeln('<info>Command is created with success.</info>');
+
+            return Command::SUCCESS;
+        } catch (\Throwable $e) {
+            $output->writeln('<error>' . $e->getMessage() . '</error>');
+
+            return Command::FAILURE;
+        }
+    }
+
+    #[\Override]
+    protected function configure(): void
+    {
+        // in the "future", `creationDate` maybe will be got from another service
+        $this->addOption('creationDate', 'c', InputOption::VALUE_OPTIONAL, sprintf('Creation date of the order (%s)', self::DATE_FORMAT));
+    }
+
+    private function checkCreationDate(mixed $creationDate): \DateTimeImmutable
+    {
+        if (!isset($creationDate)) {
+            return new \DateTimeImmutable();
+        }
+
+        if (!\is_string($creationDate)) {
+            throw new \InvalidArgumentException('Creation date must be a string.');
+        }
+
+        $creationDateTime = \DateTimeImmutable::createFromFormat(format: self::DATE_FORMAT, datetime: $creationDate);
+        if (!$creationDateTime instanceof \DateTimeImmutable) {
+            throw new \InvalidArgumentException(sprintf('Invalid creation date format. Please provide a date in the format %s', self::DATE_FORMAT));
+        }
+
+        return $creationDateTime;
     }
 }
--- srv/app/tests/phpunit/UserInterface/Worker/Order/CreateOrder/CreateOrderWorkerTest.php
+++ srv/app/tests/phpunit/UserInterface/Worker/Order/CreateOrder/CreateOrderWorkerTest.php
@@ -1,39 +1,54 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Tests\PHPUnit\UserInterface\Worker\Order\CreateOrder;
+
 use App\Infrastructure\Persistence\Doctrine\DBAL\Table\Write\OrderEventTable;
-use App\Tests\PHPUnit\Shared\TestCase\ApiTestCase;
 use App\Tests\PHPUnit\Shared\TestCase\MessagingTestCase;
 use App\UserInterface\Worker\Order\CreateOrder\CreateOrderPayload;
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Zenstruck\Messenger\Test\InteractsWithMessenger;
+use Doctrine\DBAL\Exception;
 
 class CreateOrderWorkerTest extends MessagingTestCase
 {
+    private const string ORDER_REFERENCE = 'reference1';
+    private const string LOADING_IDENTIFIER = 'place_123';
+    private const string DELIVERY_IDENTIFIER = 'place_456';
+    private const string CREATED_AT = '2024-03-19T15:00:00+00:00';
 
-    private const ORDER_REFERENCE= 'reference1';
-    private const LOADING_IDENTIFIER = 'place_123';
-    private const DELIVERY_IDENTIFIER = 'place_456';
-    private const CREATED_AT = '2024-03-19T15:00:00+00:00';
-
-    public function testHandleCreateOrderPayload()
+    /**
+     * @throws Exception
+     */
+    public function testHandleCreateOrderPayload(): void
     {
+        $createDate = \DateTimeImmutable::createFromFormat(\DATE_ATOM, self::CREATED_AT);
+
         $payload = new CreateOrderPayload(
             reference: self::ORDER_REFERENCE,
             loadingIdentifier: self::LOADING_IDENTIFIER,
             deliveryIdentifier: self::DELIVERY_IDENTIFIER,
-            creationDate: \DateTimeImmutable::createFromFormat(\DATE_ATOM, self::CREATED_AT)
+            creationDate: $createDate
         );
 
         $this->transport()->send($payload);
         $this->transport()->process();
-        $orderId = $this->connectionService()->createQueryBuilder()
+
+        $order = $this->connectionService()->createQueryBuilder()
             ->select(OrderEventTable::COLUMN_AGGREGATE_ROOT_ID)
+            ->addSelect(OrderEventTable::COLUMN_EVENT_NAME)
+            ->addSelect(OrderEventTable::COLUMN_OCCURRED_ON)
             ->from(OrderEventTable::TABLE_NAME)
             ->executeQuery()
-            ->fetchOne()
-            ;
+            ->fetchAssociative()
+        ;
 
-        $this->assertNotFalse($orderId);
+        self::assertEquals(
+            expected: [
+                'aggregate_root_id' => 'f8cea8fd-a687-40d6-8959-9160fab35ec3',
+                'occurred_on' => '2024-03-19 15:00:00+00',
+                'event_name' => 'order_created',
+            ],
+            actual: $order
+        );
     }
 }
